7b8df0bb210e7d0a3fd2fa9850ee45accbfbf8b1
Ethan <eduvall23@turing.edu>
2025-07-19 22:39:21 -0600

    Working character form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -12,20 +12,7 @@ function App() {
 
   function getCharacters() {
     //wip until i create my own api
-    var htmlCharacters = [];
-    characters.forEach((character) => {
-      htmlCharacters.push(
-        <div
-          id={character.id}
-          className={`rarity${character.rarity} characters`}
-        >
-          <img src={character.icon} alt={`Icon of ${character.name}`} />
-          <h3>{character.name}</h3>
-          <p>{character.element}</p>
-        </div>
-      );
-    });
-    setAllCharacters(htmlCharacters);
+    setAllCharacters(characters);
   }
 
   function characterForm() {
@@ -36,11 +23,13 @@ function App() {
           <input
             type="checkbox"
             id={character.id}
+            name="characters"
             className={`rarity${character.rarity} characters`}
-            value={character.name}
+            value={character.id}
           />
 
-          <label for={character.id}>
+          <label htmlFor={character.id}>
+            {<h3>{character.name}</h3>}
             {<img src={character.icon} alt={`Icon of ${character.name}`} />}
           </label>
         </>
@@ -51,12 +40,40 @@ function App() {
 
   function displayCharacters() {
     if (ownedCharacters) {
-      return <h1>hi</h1>;
+      const displayedCharacters = ownedCharacters.map((selectedCharacter) => {
+        const foundCharacter = allCharacters.find((character) => {
+          return character.id == selectedCharacter;
+        });
+        if (foundCharacter) {
+          return (
+            <div
+              id={foundCharacter.id}
+              className={`rarity${foundCharacter.rarity} characters`}
+            >
+              <img
+                src={foundCharacter.icon}
+                alt={`Icon of ${foundCharacter.name}`}
+              />
+              <h3>{foundCharacter.name}</h3>
+              <p>{foundCharacter.element}</p>
+            </div>
+          );
+        }
+      });
+
+      return displayedCharacters;
     } else {
       return <h2> No characters found - add them or refresh the page </h2>;
     }
   }
 
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    setIsModalOpen(false);
+    const formData = new FormData(e.target);
+    const selected = formData.getAll("characters");
+    setOwnedCharacters(selected);
+  }
   return (
     <div>
       <h1>Genshin Team Builder!</h1>
@@ -64,10 +81,10 @@ function App() {
       <div>{displayCharacters()}</div>
       <button
         onClick={() => {
-          if(isModalOpen){
-            setIsModalOpen(false)
-          }else{
-            setIsModalOpen(true)
+          if (isModalOpen) {
+            setIsModalOpen(false);
+          } else {
+            setIsModalOpen(true);
           }
         }}
       >
@@ -75,7 +92,16 @@ function App() {
       </button>
 
       <button>Create me a team!</button>
-      {isModalOpen && <form>{characterForm()}</form>}
+      {isModalOpen && (
+        <form
+          onSubmit={(e) => {
+            handleFormSubmit(e);
+          }}
+        >
+          {characterForm()}
+          <input type="submit" />
+        </form>
+      )}
     </div>
   );
 }
