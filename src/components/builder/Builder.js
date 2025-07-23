b041f46b7a694add3792b4ea36f1ca02a70bbe2a
Ethan <eduvall23@turing.edu>
2025-07-22 19:32:41 -0600

    getting ready for the final function

--- src/components/builder/Builder.js
+++ src/components/builder/Builder.js
@@ -1,27 +1,47 @@
 import "./Builder.css";
 import { useEffect, useState } from "react";
-
-function Builder(chosenCharacter) {
+import fetchedTeams from "../../util/teams.json";
+function Builder({ chosenCharacter }) {
   const [teams, setTeams] = useState(null);
-
-
+  const [generatedTeams, setGeneratedTeams] = useState(null)
 
   useEffect(() => {
-    fetchTeams(chosenCharacter)
+    fetchTeams(chosenCharacter);
+    generateTeam()
   }, []);
 
-
-
   function fetchTeams(character) {
-    //Placeholder until api is created it will use the same logic without having to search an array 
+    //Placeholder until api is created it will use the same logic without having to search an array
+    setTeams(fetchedTeams.find((team) => team.id === character.id));
     
+  }
+  function generateTeam() {
 
-  } 
-  function generateTeam() {}
+
+
+
+
+
+
+
+
+
+  }
 
   return (
     <div>
-      <h1>Hi</h1>
+      <div
+        id={chosenCharacter.id}
+        className={`rarity${chosenCharacter.rarity} characters`}
+      >
+        <img
+          src={chosenCharacter.icon}
+          alt={`Icon of ${chosenCharacter.name}`}
+        />
+        <h3>{chosenCharacter.fieldRole}</h3>
+        <h3>{chosenCharacter.name}</h3>
+        <p>{chosenCharacter.element}</p>
+      </div>
     </div>
   );
 }
